ff254b3df6190c2de9be159b1e5c9ad1d021b6c6
AL <lxy081314@gmail.com>
2021-02-21 23:31:07 -0500

    unit2

---
+++ Week1-TableView/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by lxy on 2/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Week1-TableView/MovieViewController.swift
+++ Week1-TableView/MovieViewController.swift
@@ -65,20 +65,30 @@ class MovieViewController: UIViewController,UITableViewDataSource, UITableViewDe
         let posterPath=movie["poster_path"] as!String
         let posterUrl=URL(string: baseUrl+posterPath)
         
-        cell.posterView.af_setImage(withURL:posterUrl!)
+        cell.posterView.af.setImage(withURL:posterUrl!)
         
         return cell
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        //find selected movie
+        let cell=sender as!UITableViewCell
+        let indexPath=tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        
+        //pass it to detailViewController
+        let detailsViewController=segue.destination as! MovieDetailsViewController
+        detailsViewController.movie=movie
+        tableView.deselectRow(at: indexPath, animated: true)//so when back to movies, selected history no showing
     }
-    */
+    
 
 }
